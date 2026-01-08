2bc7bcf168286962fb3dd75b9dc35fd6a23b725f
codefaux <codefaux@solacenet.org>
2026-01-08 11:51:17 -0800

    Initial commit, fork from codefaux/webhook-acquisition-initiator

---
+++ build/lib/cfsonarr/__init__.py
@@ -0,0 +1,7 @@
+from .cfsonarr import (get_all_series, get_episode_data_for_shows,
+                       get_episodes, get_file_quality_and_language,
+                       get_monitored_series, get_sonarr_episode,
+                       import_downloaded_episode, is_episode_file,
+                       is_monitored_episode, is_monitored_series,
+                       refresh_series, search_local_series, search_series,
+                       validate_sonarr_config)
